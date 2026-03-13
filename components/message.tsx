a0894322a7d5698d6bbc31ab048b8fb7cdd502a5
Meg <msi32@cornell.edu>
2026-03-13 16:59:41 -0400

    Reapply "Update color scheme"
    This reverts commit 8d775a0673b4028490e9e3e763147d242d7438dd.

--- components/message.tsx
+++ components/message.tsx
@@ -144,7 +144,7 @@ const PurePreviewMessage = ({
                   <div key={key}>
                     <MessageContent
                       className={cn({
-                        "wrap-break-word w-fit rounded-2xl px-3 py-2 text-right text-white":
+                        "wrap-break-word w-fit rounded-2xl px-3 py-2 text-right text-[#ece7e6]":
                           message.role === "user",
                         "bg-transparent px-0 py-0 text-left":
                           message.role === "assistant",
@@ -152,7 +152,7 @@ const PurePreviewMessage = ({
                       data-testid="message-content"
                       style={
                         message.role === "user"
-                          ? { backgroundColor: "#006cff" }
+                          ? { backgroundColor: "#550000" }
                           : undefined
                       }
                     >
